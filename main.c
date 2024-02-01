b307017bf28ba6abb020ea5b6c7c3454ef8dd860
coltkain <kain.coltx@gmail.com>
2024-02-02 00:21:23 +0200

    feat: implement the bare bones of adding a task

--- main.c
+++ main.c
@@ -1,9 +1,10 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 void display_options()
 {
-  printf("0. Quit program\n");
+  printf("0. Save and quit program\n");
   printf("1. Add a new task item\n");
   printf("2. Remove an existing task item\n");
   printf("3. Display the task list\n");
@@ -11,9 +12,25 @@ void display_options()
   printf("\n");
 }
 
+void add_task(const char *new_task)
+{
+  FILE *to_do_list = fopen("to_do_list.txt", "a");
+
+  if (to_do_list == NULL)
+  {
+    perror("Error opening file");
+    exit(EXIT_FAILURE);
+  }
+
+  fprintf(to_do_list, "- %s\n", new_task);
+
+  fclose(to_do_list);
+}
+
 int main()
 {
   int user_choice = -1;
+
   while (user_choice != 0)
   {
     display_options();
@@ -31,6 +48,8 @@ int main()
     else
       printf("\n");
 
+    while (getchar() != '\n');
+
     switch (user_choice)
     {
       default:
@@ -39,6 +58,30 @@ int main()
 
       case 0:
         printf("Exiting program.\n\n");
+        break;
+
+      case 1:
+        const int TASK_MAX_LENGTH = 50;
+
+        char new_task[TASK_MAX_LENGTH];
+        
+        printf("New task: ");
+        if (fgets(new_task, TASK_MAX_LENGTH, stdin) == NULL)
+        {
+          perror("Error reading input");
+          exit(EXIT_FAILURE);
+        }
+        else
+          printf("\n");
+
+        size_t new_task_length = strlen(new_task);
+        if (new_task_length > 0 && new_task[new_task_length - 1] == '\n')
+          new_task[new_task_length - 1] = '\0';
+        else
+          while (getchar() != '\n');
+
+        add_task(new_task);
+
         break;
     }
   }
